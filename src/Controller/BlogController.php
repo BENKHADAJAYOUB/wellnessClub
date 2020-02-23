48029a5c631d7d4ca5b813a5805e73900453bec6
Kim <kimberlydawnbrown242@gmail.com>
2020-02-23 11:18:39 +0100

    included GET request to RSS

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Sodium\add;
+use Symfony\Component\HttpClient\HttpClient;
 
 class BlogController extends AbstractController
 {
@@ -20,13 +21,39 @@ class BlogController extends AbstractController
      */
     public function homepage(): Response
     {
+        $client = HttpClient::create();
+        $response = $client->request('GET', 'https://www.e-sante.fr/taxonomy/term/615089/feed');
+        $statusCode = $response->getStatusCode();
+        $xml=simplexml_load_string($response->getContent()) or die("Error: Cannot create object");
+        $title1 = $xml->channel[0]->item[0]->title;
+        $link1 = $xml->channel[0]->item[0]->link;
+        $title2 = $xml->channel[0]->item[1]->title;
+        $link2 = $xml->channel[0]->item[1]->link;
+        $title3 = $xml->channel[0]->item[2]->title;
+        $link3 = $xml->channel[0]->item[2]->link;
+        $title4 = $xml->channel[0]->item[3]->title;
+        $link4 = $xml->channel[0]->item[3]->link;
 
-        $liens = array("article 1","article 2","article 3 ");
-
+        $articles = [
+          [$title1, $link1],
+          [$title2, $link2],
+          [$title3, $link3],
+          [$title4, $link4],
+        ];
         return $this->render('home/linkPage.html.twig',
-                                array ('articles'=>$liens)
+                                array ('articles'=>$articles, 'fluxRSS'=>$response->getContent(),
+                                'statusCode'=>$statusCode, 'title1'=>$title1)
                                 );
 
+    }
+
+    /**
+     * @Route("/show/{id}", name="wellness_get_article")
+     */
+    public function getArticle(string $id): Response
+    {
+        return $this->render('articles/articlePage.html.twig',
+                              array ('id'=>$id));
     }
 
     /**
